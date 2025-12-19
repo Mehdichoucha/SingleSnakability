08afc5344f5bebbaff39f50e7c9c6e147241cea1
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-12-19 09:46:11 +0100

    Attributes done.

---
+++ GameState.cpp
@@ -0,0 +1,8 @@
+class GameState {
+    private:
+        int score;
+        int direction; // 1 = N, 2 = E, 3 = S, 4 = W
+        int level;
+        //Level level; //Niveau
+        //Instance de level ... jsp
+};
